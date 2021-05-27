04f7298d9ba3cd0c06903d2de893786ae4dc1814
Wilden Ali <wildenwildenaliali@gmail.com>
2021-05-27 16:45:59 +0700

    PWM - freq of pwm can adjust from 1Hz up to kHertz

--- 02-PWMLed/a-pwm_led.py
+++ 02-PWMLed/a-pwm_led.py
@@ -4,10 +4,10 @@ led_pin = 18
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(led_pin, GPIO.OUT)
 
-pwm_led = GPIO.PWM(led_pin, 500)
+pwm_led = GPIO.PWM(led_pin, 1000)    # 1000 Hz adalah freq
 pwm_led.start(100)
 
 while True:
     duty_s = raw_input(" Masukan nilai kecerahan (0 sampai 100): ")
-    duty = int(duty_s)
+    duty = int(duty_s)      # duty cycle dari 0% sampai 100%
     pwm_led.ChangeDutyCycle(duty)
